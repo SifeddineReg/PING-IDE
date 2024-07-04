5fff7e4ba3b8d7d65f3b83f065c6ff34f5fc1faf
frederic dong <frederic.dong@epita.fr>
2024-07-04 17:22:16 +0200

    Feat: Added the server for db

---
+++ ping_frontend/src/database/server_database.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import { getEmploye, getTask, getFiles_fromid, getFiles_frompath, updateFile, updateTask }  from '../database/database.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// GET endpoints
+app.get('/employe/:id', async (req, res) => {
+  const id = req.params.id;
+  const employe = await getEmploye(id);
+  if (employe) {
+    return employe;
+  } else {
+    res.status(404).send('Employe not found');
+  }
+});
+
+app.get('/task/:id', async (req, res) => {
+  const id = req.params.id;
+  const task = await getTask(id);
+  if (task) {
+    res.json(task);
+  } else {
+    res.status(404).send('Task not found');
+  }
+});
+
+app.get('/files/:id', async (req, res) => {
+  const id = req.params.id;
+  const file = await getFiles_fromid(id);
+  if (file) {
+    res.json(file);
+  } else {
+    res.status(404).send('File not found');
+  }
+});
+
+app.get('/files/path/:path', async (req, res) => {
+  const path = req.params.path;
+  const file = await getFiles_frompath(path);
+  if (file) {
+    res.json(file);
+  } else {
+    res.status(404).send('File not found');
+  }
+});
+
+// UPDATE endpoints
+app.put('/file/:id', async (req, res) => {
+  const id = req.params.id;
+  const { new_path, userID } = req.body;
+  const file = await updateFile(id, new_path, userID);
+  if (file) {
+    res.json(file);
+  } else {
+    res.status(404).send('File not found');
+  }
+});
+
+app.put('/task/:id', async (req, res) => {
+  const id = req.params.id;
+  const { new_state } = req.body;
+  const task = await updateTask(id, new_state);
+  if (task) {
+    res.json(task);
+  } else {
+    res.status(404).send('Task not found');
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server listening at http://localhost:${port}");
+});
--- ping_frontend/vite.config.ts
+++ ping_frontend/vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      },
+      '/db':{
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/db/, ''),
+        secure: false,
+      }
+    }
+  }
 })
